3efe2ff94254d796ffc0d7a99b7be526e3d003c0
wilson3centaurus <tafadzwawilsonsedze@gmail.com>
2024-04-03 17:33:56 +0200

    update

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class User(BaseModel):
+    username: str
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    return {}
